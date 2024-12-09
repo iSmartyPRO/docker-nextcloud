691338ef55db166290d45d7d8d23649deabd2147
root <root@prb-docker.GESCONS.RU>
2024-12-09 06:34:08 +0000

    Improvement - added login and password

--- test-ldap.sh
+++ test-ldap.sh
@@ -23,14 +23,6 @@ if [ -z "$LDAP_HOST" ] || [ -z "$LDAP_PORT" ] || [ -z "$LDAP_BASE" ] || [ -z "$L
 fi
 
 # Устанавливаем конфигурацию LDAP с корректным фильтром
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ app:enable user_ldap
-# docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:create-empty-config
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:set-config s01 ldapHost "$LDAP_HOST"
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:set-config s01 ldapPort "$LDAP_PORT"
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:set-config s01 ldapBase "$LDAP_BASE"
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:set-config s01 ldapAgentName "$LDAP_AGENTNAME"
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:set-config s01 ldapAgentPassword "$LDAP_AGENTPASSWORD"
-docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:set-config s01 ldapLoginFilter "(&(objectClass=person)(uid=%uid))"
 docker exec -u 33 "$DOCKER_CONTAINER_NAME" php occ ldap:test-config s01
 
 if [ $? -eq 0 ]; then
